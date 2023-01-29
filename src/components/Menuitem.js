1d3b35920b875cb7661b0510d6f866c2b5bbd708
AarmST15 <armmy.2001@gmail.com>
2023-01-29 13:33:49 +0700

    y

--- src/components/Menuitem.js
+++ src/components/Menuitem.js
@@ -1,4 +1,40 @@
-import React from 'react'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Menupopup from './Menupopup.js';
+
+export default function ActionAreaCard() {
+  return (
+    <div className='card-item'>
+    <Card sx={{ maxWidth: 300 }}>
+      <CardActionArea >
+        <CardMedia
+          component="img"
+          height="200"
+          image="./images/coffee-01.jpg"
+          alt="coffee"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Espresso
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          Espresso is straight coffee concentrate, usually enjoyed straight or with a sprinkle of sugar. 
+          It has a full, strong coffee flavor and is known for its intensity
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    <Menupopup/>
+    </div>
+  );
+}
+
+
+/*import React from 'react'
 import './Menuitem.css'
 
 function Menuitem() {
@@ -10,4 +46,4 @@ function Menuitem() {
   )
 }
 
-export default Menuitem
+export default Menuitem*/
