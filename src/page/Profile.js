8411c29d1920e46ae442b5fe8691b22e46f7ee70
AarmST15 <armmy.2001@gmail.com>
2023-03-07 11:11:01 +0700

    y

--- src/page/Profile.js
+++ src/page/Profile.js
@@ -1,13 +1,25 @@
-import React from 'react';
 import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 import './Profile.css'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? 'rgba(0, 0, 0, 0.0)' : 'rgba(0, 0, 0, 0.0)',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export default function Profile() {
   return (
     <section className="vh-100" style={{ backgroundColor: '#f4f5f7' }}>
       <MDBContainer className="py-5 h-100">
         <MDBRow className="justify-content-center align-items-center h-100">
-          <MDBCol lg="6" className="mb-4 mb-lg-0">
+          <MDBCol lg="8" className="mb-4 mb-lg-0">
             <MDBCard className="mb-3" style={{ borderRadius: '.5rem' }}>
               <MDBRow className="g-0">
                 <MDBCol md="4" className="gradient-custom text-center text-white"
@@ -18,10 +30,11 @@ export default function Profile() {
                   <MDBCardText>Web Designer</MDBCardText>
                   <MDBIcon far icon="edit mb-5" />
                 </MDBCol>
+                    
                 <MDBCol md="8">
-                  <MDBCardBody className="p-4">
+                <Box sx={{ flexGrow: 1 }} style={{margin:"35px"}}>
                     <MDBTypography tag="h6">Information</MDBTypography>
-                    <hr className="mt-0 mb-4" />
+                    <hr className="mt-0 mb-" />
                     <MDBRow className="pt-1">
                       <MDBCol size="6" className="mb-3">
                         <MDBTypography tag="h6">Email</MDBTypography>
@@ -33,18 +46,15 @@ export default function Profile() {
                       </MDBCol>
                     </MDBRow>
 
-                    <MDBTypography tag="h6">Score</MDBTypography>
-                    <hr className="mt-0 mb-4" />
-                    <MDBRow className="pt-1">
+                    <MDBTypography tag="h6">Point</MDBTypography>
+                    <hr className="mt-0 mb-4" />                    
+                        <MDBTypography tag="h6">16235</MDBTypography>
+                        <MDBCardText className="text-muted">How to use point.</MDBCardText>
                       
-                    </MDBRow>
+                    
 
-                    <div className="d-flex justify-content-start">
-                      <a href="#!"><MDBIcon fab icon="facebook me-3" size="lg" /></a>
-                      <a href="#!"><MDBIcon fab icon="twitter me-3" size="lg" /></a>
-                      <a href="#!"><MDBIcon fab icon="instagram me-3" size="lg" /></a>
-                    </div>
-                  </MDBCardBody>
+                    
+                  </Box>
                 </MDBCol>
               </MDBRow>
             </MDBCard>
