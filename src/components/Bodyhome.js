d8591b565678b0f6ea5c3a33c71b4ed10201b540
AarmST15 <armmy.2001@gmail.com>
2023-03-15 23:20:57 +0700

    y

--- src/components/Bodyhome.js
+++ src/components/Bodyhome.js
@@ -1,19 +1,49 @@
 import React from 'react';
-import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import { MDBContainer,MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 import './Bodyhome.css'
 
 export default function Bodyhome() {
   return (    
-    <MDBRow className='grid-stye'>
-      <MDBCol md='4' className='img-stye'>
-        <img class="overflow-hidden" style={{height:"400px" ,width:"400px" }} src='./images/Bodyhome-2.jpg'/>
-      </MDBCol>
-      <MDBCol md='7' className='text-stye'>
-        <h4>Drink it and enjoy it.</h4>
-        First I drink the coffee. Then I do the things       
-      </MDBCol>
-    </MDBRow>
-       
+    <div>
+    
+        {/* <MDBRow >
+            <MDBCol xs='4' md='4' className='img-stye'>
+                <img class="overflow-hidden" style={{height:"220px" ,width:"400px" }} src='./images/Bodyhome-4.jpg'/>
+            </MDBCol>
+            <MDBCol xs='8' md='8' className='text-stye'>
+                <h4>Drink it and enjoy it.</h4>
+                First I drink the coffee. Then I do the things       
+            </MDBCol>
+        </MDBRow> */}
+        <Grid container spacing={2} justifyContent="center"
+  alignItems="center">
+        <Grid item md="4" xs="auto" >
+            <img class="overflow-hidden" style={{height:"220px" ,width:"400px" }} src='./images/Bodyhome-4.jpg'/>
+            </Grid>
+            <Grid item xs="auto" md="8">
+            <h4>Drink it and enjoy it.</h4>
+                First I drink the coffee. Then I do the things  
+                </Grid>
+        </Grid>
+    
+    <MDBRow  className='row-cols-1 '>
+        {/* <MDBCol xl='4'>
+          xl="4"
+        </MDBCol>
+        <MDBCol xl='8' >
+          xl="4"
+        </MDBCol> */}
+        
+      </MDBRow>
+   
+
+
+
+
+      </div> 
 
   );
 }
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,39 +3,72 @@ import {
   MDBCarousel,
   MDBCarouselItem,
 } from 'mdb-react-ui-kit';
+import './Carousel.css'
 
 export default function Carousel() {
   return (
-    <MDBCarousel showIndicators showControls fade>
-      <MDBCarouselItem
-        className='w-100 d-block'
-        itemId={1}
-        src='./images/carousel-01.jpg'
-        alt='...'
-      >
-        <h5>First slide label</h5>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </MDBCarouselItem>
+    <div>
+    {/* <MDBCarousel showIndicators showControls fade>
+    <MDBCarouselItem
+      className='w-100 d-block'
+      itemId={1}
+      src='./images/carousel-01.jpg'
+      alt='...'
+    >
+      <h5>First slide label</h5>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </MDBCarouselItem>
 
-      <MDBCarouselItem
-        className='w-100 d-block'
-        itemId={2}
-        src='./images/carousel-02.jpg'
-        alt='...'
-      >
-        <h5>Second slide label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </MDBCarouselItem>
+    <MDBCarouselItem
+      className='w-100 d-block'
+      itemId={2}
+      src='./images/carousel-02.jpg'
+      alt='...'
+    >
+      <h5>Second slide label</h5>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </MDBCarouselItem>
 
-      <MDBCarouselItem
-        className='w-100 d-block'
-        itemId={3}
-        src='./images/carousel-03.jpg'
-        alt='...'
+    <MDBCarouselItem
+      className='w-100 d-block'
+      itemId={3}
+      src='./images/carousel-03.jpg'
+      alt='...'
+    >
+      <h5>Third slide label</h5>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </MDBCarouselItem>
+  </MDBCarousel> */}
+  <MDBCarousel  showControls fade>
+    <MDBCarouselItem
+      className='w-100 carousel-bg'
+      itemId={1}
+      src='./images/carousel-04.jpg'      
       >
-        <h5>Third slide label</h5>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </MDBCarouselItem>
-    </MDBCarousel>
+      <div>
+      <h5 className='mb-3'>First slide label</h5>
+      <p className='mb-3'>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </MDBCarouselItem>
+
+    <MDBCarouselItem
+      className='w-100 carousel-bg'
+      itemId={2}
+      src='./images/carousel-04.jpg'      
+    >
+      <h5 className='mb-3'>Second slide label</h5>
+      <p className='mb-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </MDBCarouselItem>
+
+    <MDBCarouselItem
+      className='w-100 carousel-bg'
+      itemId={3}
+      src='./images/carousel-04.jpg'      
+    >
+      <h5 className='mb-3'>Third slide label</h5>
+      <p className='mb-3'>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </MDBCarouselItem>
+  </MDBCarousel>
+</div>
   );
 }
